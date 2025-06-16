429e985e9bdf513528a8d072c79dae64140cce87
cjbarre <cjbarre@gmail.com>
2025-06-16 09:48:59 -0700

    Add now-from-ms to time utils.

--- components/time/src/ai/obney/grain/time/interface.clj
+++ components/time/src/ai/obney/grain/time/interface.clj
@@ -3,3 +3,8 @@
 (defn now [] (java.time.OffsetDateTime/now))
 
 (defn now-from-str [s] (java.time.OffsetDateTime/parse s))
+
+(defn now-from-ms [ms]
+  (java.time.OffsetDateTime/ofInstant
+   (java.time.Instant/ofEpochMilli ms)
+   java.time.ZoneOffset/UTC))
