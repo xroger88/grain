680ddba7e25ea9214d18ee9603b761f08ce4d923
cjbarre <cjbarre@gmail.com>
2025-08-19 10:00:11 -0700

    Fix swapped status codes.

--- components/command-request-handler/src/ai/obney/grain/command_request_handler/core.clj
+++ components/command-request-handler/src/ai/obney/grain/command_request_handler/core.clj
@@ -32,12 +32,12 @@
 
 (defmethod process-command-result ::anom/forbidden
   [{:keys [::anom/message]}]
-  {:status 409
+  {:status 403
    :body {:message message}})
 
 (defmethod process-command-result ::anom/conflict
   [{:keys [::anom/message]}]
-  {:status 403
+  {:status 409
    :body {:message message}})
 
 (defmethod process-command-result :default
